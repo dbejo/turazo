a36762e95f14f338e4288aad3891ffbd39a7d439
dbejo <bejodavid123@gmail.com>
2024-08-23 11:36:35 +0200

    Add date support for urls

--- turazoproject/core/urls.py
+++ turazoproject/core/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.core_index, name="core_index"),
-    path("cikk/<slug:slug>/", views.core_post, name="core_post"),
+    path("cikk/<int:year>/<int:month>/<int:day>/<slug:slug>/", views.core_post, name="core_post"),
+    path("cikk/<int:year>/<int:month>/<int:day>/", views.core_day, name="core_post_day"),
+    path("cikk/<int:year>/<int:month>/", views.core_month),
+    path("cikk/<int:year>/", views.core_year),
+    path("cikk/", views.redirect_view),
     path("kategoriak/<slug:slug>/", views.core_categories, name="core_categories"),
     path("ujdonsagok/", views.core_news, name="core_news")
 ]
--- turazoproject/core/views.py
+++ turazoproject/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import Post, Category
 import random
 from django.core.paginator import Paginator
@@ -14,8 +14,8 @@ def core_index(request):
     }
     return render(request, "core/index.html", context)
 
-def core_post(request, slug):
-    post = Post.objects.get(slug=slug)
+def core_post(request, year, month, day, slug):
+    post = Post.objects.get(created_on__year=year, created_on__month=month, created_on__day=day, slug=slug)
     news = Post.objects.all().exclude(pk=post.pk).order_by("-created_on")[:4]
     related_posts = list(Post.objects.filter(category=post.category, banner=True).exclude(pk=post.pk))
     if len(related_posts) < 6 :
@@ -34,8 +34,10 @@ def core_categories(request, slug):
     p = Paginator(Post.objects.filter(category=category), 10)
     page = request.GET.get('oldal')
     posts = p.get_page(page)
+    title = category.name
     context = {
         "posts": posts,
+        "title": title
     }
     return render(request, "core/list.html", context)
 
@@ -43,7 +45,44 @@ def core_news(request):
     p = Paginator(Post.objects.all().order_by("-created_on"), 10)
     page = request.GET.get('oldal')
     posts = p.get_page(page)
+    title = "Újdonságok"
     context = {
         "posts": posts,
+        "title": title
     }
     return render(request, "core/list.html", context)
+
+def core_day(request, year, month, day):
+    p = Paginator(Post.objects.all().filter(created_on__year=year, created_on__month=month, created_on__day=day).order_by("-created_on"), 10)
+    page = request.GET.get('oldal')
+    posts = p.get_page(page)
+    title = str(year) + " " + str(month) + " " + str(day)
+    context = {
+        "posts": posts,
+        "title": title
+    }
+    return render(request, "core/list.html", context)
+
+def core_month(request, year, month):
+    p = Paginator(Post.objects.all().filter(created_on__year=year, created_on__month=month).order_by("-created_on"), 10)
+    page = request.GET.get('oldal')
+    posts = p.get_page(page)
+    title = str(year) + " " + str(month)
+    context = {
+        "posts": posts,
+        "title": title
+    }
+    return render(request, "core/list.html", context)
+
+def core_year(request, year):
+    p = Paginator(Post.objects.all().filter(created_on__year=year).order_by("-created_on"), 10)
+    page = request.GET.get('oldal')
+    posts = p.get_page(page)
+    context = {
+        "posts": posts,
+        "title": str(year)
+    }
+    return render(request, "core/list.html", context)
+
+def redirect_view(request):
+    return redirect('/')
